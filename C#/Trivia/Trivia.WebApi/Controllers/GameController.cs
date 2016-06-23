599d9475973bde079f3c8bdf3c2fddad7a1abe7c
Clément Bouillier <clement.bouillier@devcrafting.com>
2016-06-23 14:10:08 +0200

    Add a first Get endpoint to start a Trivia Game

--- C#/Trivia/Trivia.WebApi/Controllers/GameController.cs
+++ C#/Trivia/Trivia.WebApi/Controllers/GameController.cs
@@ -1,8 +1,20 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using UglyTrivia;
 
 namespace Trivia.WebApi.Controllers
 {
     public class GameController : ApiController
     {
+        private static Dictionary<string, Game> gamesInProgress = new Dictionary<string, Game>();
+
+        [HttpGet]
+        public string New()
+        {
+            var gameGuid = Guid.NewGuid().ToString();
+            gamesInProgress[gameGuid] = new Game(6, new Questions(new [] { "Sports", "Rock", "Science", "Pop"}));
+            return gameGuid;
+        }
     }
 }
--- C#/Trivia/Trivia.WebApi/Program.cs
+++ C#/Trivia/Trivia.WebApi/Program.cs
@@ -8,12 +8,12 @@ namespace Trivia.WebApi
     {
         static void Main(string[] args)
         {
-            var baseAddress = "http://localhost:8090/api";
+            var baseAddress = "http://localhost:9000";
 
             // Start OWIN host 
             using (WebApp.Start<Startup>(url: baseAddress))
             {
-                Console.WriteLine($"Server started on {baseAddress}.");
+                Console.WriteLine($"Server started on {baseAddress}/api.");
                 Console.WriteLine("Press Enter to stop it...");
                 Console.ReadLine();
             }
