a848a2f214c4a2b9a27a9155b19f36d20a6dfc24
BOUILLIER CLEMENT <clement.bouillier@devcrafting.com>
2017-06-29 10:53:27 +0200

    Basic first Trivia API: startGame + rollDice

---
+++ C#/Trivia/Trivia.WebApi/TriviaModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Nancy;
+
+namespace Trivia.WebApi
+{
+    public class TriviaModule : NancyModule
+    {
+        private static Game _game;
+        private static WebEventDispatcher _eventDispatcher;
+
+        public TriviaModule()
+        {
+            Post["/startGame"] = StartGame;
+            Post["/rollDice"] = RollDice;
+        }
+
+        private dynamic RollDice(dynamic arg)
+        {
+            _eventDispatcher.Flush();
+            var random = new Random();
+            _game.Roll(random.Next(5) + 1);
+            return _eventDispatcher.Output;
+        }
+
+        private dynamic StartGame(dynamic o)
+        {
+            _eventDispatcher = new WebEventDispatcher();
+            var players = new Players(_eventDispatcher);
+            players.Add("Chet");
+            players.Add("Pat");
+            players.Add("Sue");
+
+            var categories = new[] { "Pop", "Science", "Sports", "Rock" };
+            _game = new Game(players, new Questions(categories, new GeneratedQuestions(), _eventDispatcher), _eventDispatcher);
+            return _eventDispatcher.Output;
+        }
+    }
+}
---
+++ C#/Trivia/Trivia.WebApi/WebEventDispatcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Trivia.WebApi
+{
+    internal class WebEventDispatcher : IDispatchEvent
+    {
+        public void Display(string message)
+        {
+            Output += message + "\n";
+        }
+
+        public void Dispatch<TEvent>(TEvent @event)
+        {
+            if (@event.GetType() == typeof(PlayerRolledDice))
+            {
+                var playerRolledDice = @event as PlayerRolledDice;
+                Display(playerRolledDice.Name + " is the current player");
+                Display("They have rolled a " + playerRolledDice.Roll);
+            }
+            else if (@event.GetType() == typeof(PlayerGetOutOfPenaltyBox))
+            {
+                Display((@event as PlayerGetOutOfPenaltyBox).Name + " is getting out of the penalty box");
+            }
+            else if (@event.GetType() == typeof(PlayerWonAGoldPoint))
+            {
+                Display("Answer was correct!!!!");
+                var playerWonAGoldPoint = @event as PlayerWonAGoldPoint;
+                Display(playerWonAGoldPoint.Name + " now has " + playerWonAGoldPoint.GoldCoins + " Gold Coins.");
+            }
+        }
+
+        public string Output { get; private set; }
+
+        public void Flush()
+        {
+            Output = string.Empty;
+        }
+    }
+}
