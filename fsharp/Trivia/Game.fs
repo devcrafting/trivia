83460e3790fb73471e8d7ab76684ad1605be8e86
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-11-02 20:07:32 +0100

    Add Location to Player record

--- fsharp/Trivia/Game.fs
+++ fsharp/Trivia/Game.fs
@@ -1,5 +1,14 @@
 module TriviaGame
 
-type Player = {
-    Name: string
-}
+type Player = 
+    {
+        Name: string
+        Location: int
+    }
+    static member WithName name =
+        { Name = name; Location = 0 }
+
+let move diceValue player =
+    let newLocation = (player.Location + diceValue) % 12
+    printfn "%s's new location is %i" player.Name newLocation
+    { player with Location = newLocation }
